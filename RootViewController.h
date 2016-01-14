bb14cb0ededce15a7d4a50650e555c986e0b96dd
Julian Lee <Julian@Julians-MacBook-Pro.local>
2016-01-14 11:40:11 -0800

    storyboard setup complete

--- RootViewController.h
+++ RootViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  RootViewController.h
 //  Calcutron
 //
 //  Created by Julian Lee on 1/14/16.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface RootViewController : UIViewController
 
 @end
-
